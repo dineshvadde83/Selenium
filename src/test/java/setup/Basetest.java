75c0d6757c8172ad7ee3747066afb5d9a89eb06c
vadde <vadde@hp>
2025-09-30 15:26:58 +0530

    new commit from branch test 5

--- src/test/java/setup/Basetest.java
+++ src/test/java/setup/Basetest.java
@@ -25,7 +25,8 @@ public class Basetest {
 		cap.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, appActivity);
 		cap.setCapability(MobileCapabilityType.NO_RESET, "true");
 		driver =new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
-		//java functions addedd in the 
+		//java functions addedd in the  base test
+		
 //		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
 		// I have modified through local branch Dinesh Test 1
