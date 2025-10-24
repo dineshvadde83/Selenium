d059e1d21af0f58cf028309eb50b5b9f699c9b31
dineshvadde83 <dineshvadde83@gmail.com>
2025-10-24 21:29:09 +0530

    pushing new code

--- src/test/java/setup/Class2.java
+++ src/test/java/setup/Class2.java
@@ -3,6 +3,7 @@ package setup;
 public class Class2 {
 
 	public static void main(String[] args) {
+		System.out.println("local to branch to reslove conflicts");
 
 //dinesh is JAVA
 		System.out.println("kavya");
@@ -12,6 +13,7 @@ public class Class2 {
 		int a =10;
 
 		System.out.println("java");
+		System.out.println("local to branch to reslove conflicts");
 	}
 
 }
--- src/test/java/setup/NewBranch.java
+++ src/test/java/setup/NewBranch.java
@@ -5,6 +5,7 @@ public class NewBranch {
 	public static void main(String[] args) {
 		//dinesh
 // just created a new file as new branch in a local repo , localautomation master 
+		
 	}
 
 }
