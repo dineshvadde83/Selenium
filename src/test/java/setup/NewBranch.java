527dc4a43448483a935e979f49a78541e087b22a
dineshvadde83 <dineshvadde83@gmail.com>
2025-10-24 21:21:42 +0530

    Revert "changes1"
    This reverts commit cc4587eec68262eb0e37ad051d010648e1eef29c.

--- src/test/java/setup/NewBranch.java
+++ src/test/java/setup/NewBranch.java
@@ -5,7 +5,6 @@ public class NewBranch {
 	public static void main(String[] args) {
 		//dinesh
 // just created a new file as new branch in a local repo , localautomation master 
-		//second commit
 	}
 
 }
