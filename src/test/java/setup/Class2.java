d5c0db2734f3a467cf045e7e47b2c8f30917fe83
dineshvadde83 <dineshvadde83@gmail.com>
2025-10-24 15:24:03 +0530

    creating new branch

---
+++ src/test/java/setup/Class2.java
@@ -0,0 +1,9 @@
+package setup;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+//dinesh is
+	}
+
+}
---
+++ src/test/java/setup/NewBranch.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class NewBranch {
+
+	public static void main(String[] args) {
+		//dinesh
+// just created a new file as new branch in a local repo , localautomation master 
+	}
+
+}
